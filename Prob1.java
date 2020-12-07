d214d3c7a62162b4dab57d1bed6f949f6b91d733
whack <aj0964@students.necsd.net>
2020-12-07 16:46:31 +0000

    prob 1 W.I.P

--- Prob1.java
+++ Prob1.java
@@ -2,7 +2,15 @@ import java.util.Scanner;
 
 public class Prob1 {
   public static void main(String[] args){
-
+ Scanner Scan = new Scanner(System.in);
+ System.out.print("Enter an interger: ");
+ int num = Scan.nextInt();
+  if (num >= 5) || (num <= 76) {
+   System.out.println("False");
+ }
+else {
+  System.out.println("True");
+}
 
 
   }
