b18545e3a0683dbec4179d0c9a1f4fef6ff85144
whack <aj0964@students.necsd.net>
2020-12-11 15:21:03 +0000

    prob3

--- Prob3.java
+++ Prob3.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class Prob3 {
   public static void main(String[] args) {
-    
+    Scanner Scan = new Scanner(System.in);
     
     // Prompt the user to enter weight in pounds
-    
-    
+    System.out.print("Enter weight in pounds: ");
+    double weight = Scan.nextDouble();
     // Prompt the user to enter height in inches
-
-
+    System.out.println("Enter height in inches: ");
+    double height = Scan.nextDouble();
     
     //Constant
     final double KILOGRAMS_PER_POUND = 0.45359237; 
@@ -21,6 +21,17 @@ public class Prob3 {
     double BMI = weightInKilograms/(heightInMeters * heightInMeters);
 
     // Display result using if, else if and else
-   
+    if (BMI < 18.5)
+    System.out.println("Underweight");
+    
+    else if (BMI < 25)
+    System.out.println("Normal");
+    
+    else if (BMI < 30) 
+    System.out.println("Overweight");
+    
+    else 
+      System.out.println("Obese");
+    
   }
 }
