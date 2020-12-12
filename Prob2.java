b3cec6d0f362ea7477b6fbe6742079cb467bb5e5
whack <aj0964@students.necsd.net>
2020-12-12 04:53:10 +0000

    prob2

--- Prob2.java
+++ Prob2.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Prob2 {
   public static void main(String[] args){
-  Scanner Scan = New Scanner(System.in);
-
+  Scanner Scan = new Scanner(System.in);
+  System.out.print("Please enter the First interger: ");
+  int num1 = Scan.nextInt();
+  System.out.print("Please enter the Second interger: ");
+  int num2 = Scan.nextInt();
+  if ((num1 >= 0) && (num2 >= 0)){
+    System.out.print("Both are positive or zero.");
+  }
+  else{
+    System.out.print("One or both are negative.");  
+  }
   }
   
   }
